7b04b94216cc8fa8c03ef8bd66a68001a2c66b20
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2022-03-27 23:46:09 -0300

    The logger dependency has been incremented and used

--- src/main/java/willfirstapi/com/ApiRestJava/controller/ApiControllers.java
+++ src/main/java/willfirstapi/com/ApiRestJava/controller/ApiControllers.java
@@ -4,17 +4,23 @@ import org.springframework.web.bind.annotation.*;
 import willfirstapi.com.ApiRestJava.entities.User;
 import willfirstapi.com.ApiRestJava.provider.EncryptPassword;
 
+import java.util.logging.Logger;
+
 @RestController
 public class ApiControllers {
+    private final Logger logger = Logger.getLogger("br.com.ApiController");
+
     @GetMapping(value = "/hash")
-    public static String hashPassword(@RequestParam(value = "password") String password) {
+    public String hashPassword(@RequestParam(value = "password") String password) {
+        logger.info("The router /hash has been executed");
         return EncryptPassword.hash(password);
     }
 
     @GetMapping(value = "/verifyhash")
-    public static boolean verifyHash(
+    public boolean verifyHash(
             @RequestParam(value = "password") String password,
             @RequestParam(value = "hash") String hash) {
+        logger.info("The router /verifyhash has been executed");
         return EncryptPassword.verifyHash(password, hash);
     }
 
